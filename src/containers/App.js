7762abceb444c3234257337d4ca9078503b9eaab
alex570 <yegupov@gmail.com>
2017-01-11 17:57:33 +0200

    removed App errorMessage warning

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,45 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { browserHistory } from 'react-router'
+//import { browserHistory } from 'react-router'
 //import Explore from '../components/Explore'
-import { resetErrorMessage } from '../actions'
+//import { resetErrorMessage } from '../actions'
 
 class App extends Component {
   static propTypes = {
     // Injected by React Redux
-    errorMessage: PropTypes.string,
-    resetErrorMessage: PropTypes.func.isRequired,
-    inputValue: PropTypes.string.isRequired,
+    // errorMessage: PropTypes.string,
+    // resetErrorMessage: PropTypes.func.isRequired,
+    // inputValue: PropTypes.string.isRequired,
     // Injected by React Router
     children: PropTypes.node
   }
 
-  handleDismissClick = e => {
-    this.props.resetErrorMessage()
-    e.preventDefault()
-  }
-
-  handleChange = nextValue => {
-    browserHistory.push(`/${nextValue}`)
-  }
-
-  renderErrorMessage() {
-    const { errorMessage } = this.props
-    if (!errorMessage) {
-      return null
-    }
-
-    return (
-      <p style={{ backgroundColor: '#e99', padding: 10 }}>
-        <b>{errorMessage}</b>
-        {' '}
-        (<a href="#"
-            onClick={this.handleDismissClick}>
-          Dismiss
-        </a>)
-      </p>
-    )
-  }
+  // handleDismissClick = e => {
+  //   this.props.resetErrorMessage()
+  //   e.preventDefault()
+  // }
+  // 
+  // handleChange = nextValue => {
+  //   browserHistory.push(`/${nextValue}`)
+  // }
+  // 
+  // renderErrorMessage() {
+  //   const { errorMessage } = this.props
+  //   if (!errorMessage) {
+  //     return null
+  //   }
+  // 
+  //   return (
+  //     <p style={{ backgroundColor: '#e99', padding: 10 }}>
+  //       <b>{errorMessage}</b>
+  //       {' '}
+  //       (<a href="#"
+  //           onClick={this.handleDismissClick}>
+  //         Dismiss
+  //       </a>)
+  //     </p>
+  //   )
+  // }
 
   render() {
     const { children, /* inputValue*/ } = this.props
@@ -56,10 +56,11 @@ class App extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  errorMessage: state.errorMessage,
-  inputValue: ownProps.location.pathname.substring(1)
+  //errorMessage: state.errorMessage,
+  //inputValue: ownProps.location.pathname.substring(1)
 })
 
-export default connect(mapStateToProps, {
-  resetErrorMessage
-})(App)
+export default connect(
+  mapStateToProps,
+  //{resetErrorMessage}
+)(App)
