3219a07fe739d24d6b7aec342e57b685b9343ba5
alex570 <yegupov@gmail.com>
2017-02-28 18:05:17 +0200

    added project moved message

--- src/settings.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiPort: process.env.NODE_API_PORT || 3001,
-  apiHost: process.env.NODE_API_HOST || 'localhost',
-  dbName: process.env.SO_DB_NAME || 'stackover',
-  dbPassword: process.env.SO_DB_PWD || 'secret',
-  dbUser: process.env.SO_DB_USER || process.env.USER,
-  //dbHost: process.env.SO_DB_HOST || 'localhost',
-}
--- src/utils/serialize.js
+++
@@ -1,2 +0,0 @@
-// convert object to url params
-export const urlize = (obj) => Object.keys(obj).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(obj[k])}`).join('&');
